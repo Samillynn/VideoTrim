8dcf8d9511763c5187c53f503de1f35f4966caf0
Mark Huang <hhzeos@gmail.com>
2020-06-18 21:48:22 +0800

    Minor Update

--- trim.py
+++ trim.py
@@ -1,38 +1,51 @@
 import os
-from os.path import join as pathjoin
 import subprocess
+from os.path import join as pathjoin
 from concurrent.futures import ProcessPoolExecutor
+
 from configparse import Parser
 from cmdparse import parse_args
 
 
 class Trimmer:
-    def __init__(self, infoler='.', outfolder='.', max_workers=4):
-        self.infolder = infoler
-        self.outfolder = outfolder
+    def __init__(self, dir_in=".", dir_out=".", max_workers=4):
+        self.dir_in = dir_in
+        self.dir_out = dir_out
         self.max_workers = max_workers
 
-    def trim_one(self, config_obj):
-        infile, t_start, t_end, outfile = config_obj
-        infile = pathjoin(self.infolder, infile)
-        if outfile not in os.listdir(self.outfolder):
-            outfile = pathjoin(self.outfolder, outfile)
-            subprocess.run(f'ffmpeg -ss {t_start} -i "{infile}" -to {t_end} -c copy -f mp4 -copyts "{outfile + ".part"}"', shell=True)
-            os.rename(outfile + '.part', outfile)
+    def perform_trim(self, job):
+        file_in, t_start, t_end, file_out = job
+        file_in = pathjoin(self.dir_in, file_in)
+        if file_out not in os.listdir(self.dir_out):
+            file_out = pathjoin(self.dir_out, file_out)
+            subprocess.run(
+                f'ffmpeg -ss {t_start} -i "{file_in}" -to {t_end} -c copy -f mp4 -copyts "{file_out + ".part"}"',
+                shell=True,
+            )
+            os.rename(file_out + ".part", file_out)
 
-    def run(self, config_lst):
+    def run(self, job_lst):
         pool = ProcessPoolExecutor(self.max_workers)
-        for config_obj in config_lst:
-            pool.submit(self.trim_one, config_obj)
+        for job in job_lst:
+            pool.submit(self.perform_trim, job)
 
 
 def main():
-    config, infolder, outfolder, max_workers = parse_args()
-    trimmer = Trimmer(infolder, outfolder, max_workers)
-    parser = Parser(infolder, config)
-    config_lst = parser.run()
-    trimmer.run(config_lst)
+    # get user inputs
+    config, dir_in, dir_out, max_workers = parse_args()
+
+    # instantiate Parser
+    parser = Parser(dir_in, config)
+
+    # get job list from Parser
+    job_lst = parser.get_job_list()
+
+    # instantiate Trimmer
+    trimmer = Trimmer(dir_in, dir_out, max_workers)
+
+    # start Trimmer
+    trimmer.run(job_lst)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
