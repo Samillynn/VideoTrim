0cbacb372b8fb42f337f448e2e179d09368f373f
Liu Renhang <61110365+Samillynn@users.noreply.github.com>
2020-06-18 17:42:10 +0800

    Add files via upload

--- configparse.py
+++ configparse.py
@@ -1,5 +1,10 @@
+import os
+import re
 from itertools import count
 from typing import Iterator, Tuple
+from collections import namedtuple
+
+ConfigObj = namedtuple('ConfigObj', 'infile t_start t_end outfile')
 
 
 def deduplicate(line: str) -> str:
@@ -8,17 +13,42 @@ def deduplicate(line: str) -> str:
     return line.replace(' ', '')
 
 
-def parse(filename: str, output_t='mp4') -> Iterator[Tuple[str, str, str, str]]:
-    """ parse a configuration file and yield tuple(infile, t_start, t_end, outfile) each time"""
-    config = open(filename).read().splitlines()
-    for oldline in config:
-        if line := deduplicate(oldline):
-            if line.startswith('#'):
-                infile = line[1:].strip()
-                basename = infile[:infile.rfind('.')]
-                counter = count(1)
-            else:
-                args = [arg for arg in line.split(',') if arg]
-                if len(args) == 2:
-                    args.append(f'{basename}-{next(counter)}.{output_t}')
-                yield tuple(infile, *args)
+class Parser:
+    def __init__(self):
+        self.infolder = None
+        self.file_lst = None
+
+    def run(self, config='config.txt', infolder='.'):
+        config_lst = list()
+        self.infolder = infolder
+        self.file_lst = os.listdir(infolder)
+        for config_obj in self.parse(config):
+            self.check(config_obj)
+            config_lst.append(config_obj)
+        return config_lst
+
+    @staticmethod
+    def parse(filename: str, output_t='mp4') -> Iterator[Tuple[str, str, str, str]]:
+        """ parse a configuration file and yield tuple(infile, t_start, t_end, outfile) each time"""
+        config = open(filename).read().splitlines()
+        for oldline in config:
+            if line := deduplicate(oldline):
+                if line.startswith('#'):
+                    infile = line[1:].strip()
+                    basename = infile[:infile.rfind('.')]
+                    counter = count(1)
+                else:
+                    args = [arg for arg in line.split(',') if arg]
+                    if len(args) == 2:
+                        args.append(f'{basename}_clip_{next(counter)}.{output_t}')
+                    yield ConfigObj(infile, *args)
+
+    def check(self, config_obj):
+        infile, t_start, t_end, oufile = config_obj
+        if infile not in self.file_lst:
+            raise FileNotFoundError(f'Can not find {infile} in input folder {self.infolder}')
+        pattern = r'^\d+$|(\d{1,2}:)?[0-6]?\d:[0-6]?\d$'
+        if not re.match(pattern, t_start):
+            raise Exception(f'{t_start} doesn\'t satisfy time format')
+        if not re.match(pattern, t_end):
+            raise Exception(f'{t_end} doesn\'t satisfy time format')
--- trim.py
+++ trim.py
@@ -1,14 +1,37 @@
+import os
+from os.path import join as pathjoin
+import subprocess
 from concurrent.futures import ProcessPoolExecutor
-from configparse import parse
+from configparse import Parser
 from cmdparse import parse_args
-from trimmer import Trimmer
+
+
+class Trimmer:
+    def __init__(self, infoler='.', outfolder='.', max_workers=4):
+        self.infolder = infoler
+        self.outfolder = outfolder
+        self.max_workers = max_workers
+
+    def trim_one(self, config_obj):
+        infile, t_start, t_end, outfile = config_obj
+        infile = pathjoin(self.infolder, infile)
+        if outfile not in os.listdir(self.outfolder):
+            outfile = pathjoin(self.outfolder, outfile)
+            subprocess.run(f'ffmpeg -ss {t_start} -i "{infile}" -to {t_end} -c copy -f mp4 -copyts "{outfile + ".part"}"', shell=True)
+            os.rename(outfile + '.part', outfile)
+
+    def run(self, config_lst):
+        pool = ProcessPoolExecutor(self.max_workers)
+        for config_obj in config_lst:
+            pool.submit(self.trim_one, config_obj)
+
 
 def main():
-    config_file, infolder, outfolder, max_workers = parse_args()
-    trimmer = Trimmer(infolder, outfolder)
-    pool = ProcessPoolExecutor(max_workers)
-    for trim_args in parse(config_file):
-        pool.submit(trimmer.trim, trim_args)
+    config, infolder, outfolder, max_workers = parse_args()
+    trimmer = Trimmer(infolder, outfolder, max_workers)
+    parser = Parser()
+    config_lst = parser.run(config, infolder)
+    trimmer.run(config_lst)
 
 
 if __name__ == '__main__':
